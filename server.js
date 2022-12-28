93a776d7bae1d2b20665c8b325a85761e4bdd79d
sparkmahadi <mahadi.development@gmail.com>
2022-12-28 23:39:53 +0600

    database connected

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const port = process.env.PORT || 5000;
+const dotenv = require('dotenv');
+const connectDB = require("./config/db");
+dotenv.config();
+
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
