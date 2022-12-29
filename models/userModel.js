84fff9e863eb9cfb16f7523600604a30016bede6
sparkmahadi <mahadi.development@gmail.com>
2022-12-29 12:36:11 +0600

    added sign up and login api functionality with encryption

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema(
   {
@@ -12,8 +13,21 @@ const userSchema = mongoose.Schema(
         "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
   },
-  { timestaps: true }
+  { timestamps: true }   
 );
+
+userSchema.methods.matchPassword = async function (enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified) {
+      next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    console.log(this.password);
+  });
 
 const User = mongoose.model("User", userSchema);
 
--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const dotenv = require('dotenv');
 const connectDB = require("./config/db");
 dotenv.config();
 
+const userRoutes = require('./routes/userRoutes');
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
 connectDB();
 const app = express();
+app.use(express.json());
+
+app.use("/api/user", userRoutes);
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.get("/", (req, res) => {
     res.send("API is running..");
   });
 
-app.listen(port, console.log(`chatbook server is running on port ${port}`));
+app.listen(port, console.log(`chatbook server is running on port ${port}`.yellow.bold));
