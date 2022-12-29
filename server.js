07ba2e19d86a46e29550c19c4f2126d931de0c70
sparkmahadi <mahadi.development@gmail.com>
2022-12-29 22:31:22 +0600

    added messages api

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ dotenv.config();
 
 const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require('./routes/chatRoutes');
+const messageRoutes = require('./routes/messageRoutes');
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 connectDB();
@@ -16,6 +17,7 @@ app.use(express.json());
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 app.use(notFound)
 app.use(errorHandler)
