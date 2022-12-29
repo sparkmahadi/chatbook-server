5847438081049d654b06db7077f078dc31ae7dd6
sparkmahadi <mahadi.development@gmail.com>
2022-12-29 15:20:04 +0600

    added chatroute and controllers

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const connectDB = require("./config/db");
 dotenv.config();
 
 const userRoutes = require('./routes/userRoutes');
+const chatRoutes = require('./routes/chatRoutes');
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 connectDB();
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.use(notFound)
 app.use(errorHandler)
