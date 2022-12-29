458c557a975c20fe0ece5e8bca95f5c5e36d922d
sparkmahadi <mahadi.development@gmail.com>
2022-12-29 13:45:53 +0600

    added middleware and jwt implementation

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 const dotenv = require('dotenv');
 const connectDB = require("./config/db");
@@ -9,6 +10,7 @@ const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 connectDB();
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/user", userRoutes);
