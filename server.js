12540bb9b92d43010e72387e5ba715fe3ef230e4
sparkmahadi <mahadi.development@gmail.com>
2022-12-31 17:55:42 +0600

    added vercel json

--- server.js
+++ server.js
@@ -26,4 +26,50 @@ app.get("/", (req, res) => {
     res.send("API is running..");
   });
 
-app.listen(port, console.log(`chatbook server is running on port ${port}`.yellow.bold));
+const server = app.listen(port, console.log(`chatbook server is running on port ${port}`));
+
+const io = require('socket.io')(server, {
+  pingTimeout: 60000,
+  cors:{
+    origin: 'http://localhost:3000'
+  }
+})
+
+io.on("connection", (socket)=>{
+  console.log("connected to socket, user");
+  socket.on("setup", (userData) => {
+    console.log(userData._id);
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("joinChat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("newMessage", (newMessageReceived) => {
+    var chat = newMessageReceived.chat;
+
+    if (!chat.users) {
+      return console.log("chat.users not defined")
+    };
+
+    chat.users.forEach((user) => {
+      console.log(user);
+      if (user._id == newMessageReceived.sender._id) return;
+      console.log(user._id);
+      
+      socket.in(user._id).emit("messageReceived", newMessageReceived);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
+
+})
